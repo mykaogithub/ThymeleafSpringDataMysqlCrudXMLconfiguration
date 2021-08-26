4b1aac535293ebbae146db4ef20702206fcf0835
Kaouthar Elidrissi Abderrahim <kaoutharidrissilink@gmail.com>
2021-08-26 02:53:09 +0200

    first commit

---
+++ src/main/java/com/kyanja/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.kyanja.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kyanja.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Product findById(long id);
+
+	void deleteById(long id);
+ 
+}
